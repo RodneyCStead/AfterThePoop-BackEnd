f69af0e745270fe1f1bb38a25da1337c823c7fd0
RodneyCStead <RodCStead@gmail.com>
2024-12-02 17:47:17 -0330

    Added entity classes, added application properties and RestServiceApplication to project

--- src/main/java/com/keyin/domain/Transactions/Transactions.java
+++ src/main/java/com/keyin/domain/Transactions/Transactions.java
@@ -1,4 +1,67 @@
 package com.keyin.domain.Transactions;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Transactions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transId;
+    private Long buyerId;
+    private Long sellerId;
+    private Long postingId;
+    private int quantity;
+    private double price;
+
+
+    public Long getTransId() {
+        return transId;
+    }
+
+    public void setTransId(Long transId) {
+        this.transId = transId;
+    }
+
+    public Long getBuyerId() {
+        return buyerId;
+    }
+
+    public void setBuyerId(Long buyerId) {
+        this.buyerId = buyerId;
+    }
+
+    public Long getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(Long sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public Long getPostingId() {
+        return postingId;
+    }
+
+    public void setPostingId(Long postingId) {
+        this.postingId = postingId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
