9455f8c556ff90431f8365f17069ca97d619ed25
RodneyCStead <RodCStead@gmail.com>
2024-12-09 02:23:20 -0330

    added cors config

--- src/main/java/com/keyin/domain/Postings/PostingController.java
+++ src/main/java/com/keyin/domain/Postings/PostingController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/fertilizer")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PostingController {
 
     @Autowired
--- src/main/java/com/keyin/domain/Product/ProductController.java
+++ src/main/java/com/keyin/domain/Product/ProductController.java
@@ -1,15 +1,13 @@
 package com.keyin.domain.Product;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/post-product")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
 
     @Autowired
