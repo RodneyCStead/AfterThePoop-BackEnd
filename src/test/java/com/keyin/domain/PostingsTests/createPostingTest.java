4a750ab5fa04d3e6335e05abf6c11f0139f79151
RodneyCStead <RodCStead@gmail.com>
2024-12-13 09:41:40 -0330

    added testing foor all endpoints for Posting Class

--- src/test/java/com/keyin/domain/PostingsTests/createPostingTest.java
+++ src/test/java/com/keyin/domain/PostingsTests/createPostingTest.java
@@ -10,10 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
